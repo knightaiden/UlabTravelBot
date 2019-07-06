eb198984952d6e54c2e6c8416a268c15bf38a37f
siwei lin <zacharylin123@gmail.com>
2019-07-06 12:39:49 +1000

    make codes more readable

--- src/main/resources/static/chatJS.js
+++ src/main/resources/static/chatJS.js
@@ -12,24 +12,22 @@ if(e.which == 13) {
 
 
 handleResponse = (response) => {
-    console.log('robot click');
-        var megContainer1 = "<div class='row message-body'><div class='col-sm-12 message-main-receiver'><div class='receiver'><div class='message-text'>";
-        var megContainer2 = "</div><span class='message-time pull-right'>";
-        var megContainer3 = "</span></div></div></div>"
-        var mesg = response.content;
-        console.log(mesg);
-        var d = new Date($.now());
-        var time = d.getDate()+"-"+(d.getMonth() + 1)+"-"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
-        $('textarea').val('');
-        $("#conversation").append(megContainer1 + mesg + megContainer2 + time + megContainer3);
-        $('#conversation').animate({
-            scrollTop: $('#conversation').get(0).scrollHeight}, 300);
+    console.log('robot response');
+    var megContainer1 = "<div class='row message-body'><div class='col-sm-12 message-main-receiver'><div class='receiver'><div class='message-text'>";
+    var megContainer2 = "</div><span class='message-time pull-right'>";
+    var megContainer3 = "</span></div></div></div>"
+    var mesg = response.content;
+    console.log(mesg);
+    var d = new Date($.now());
+    var time = d.getDate()+"-"+(d.getMonth() + 1)+"-"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
+    $('textarea').val('');
+    $("#conversation").append(megContainer1 + mesg + megContainer2 + time + megContainer3);
+    $('#conversation').animate({
+        scrollTop: $('#conversation').get(0).scrollHeight}, 300);
 }
 
 handleSubmit = () => {
     if($('textarea').val()){
-        console.log('test submit');
-    // $("<p>Hello World!</p>").prependTo("#conversation");
     var megContainer1 = "<div class='row message-body'><div class='col-sm-12 message-main-sender'><div class='sender'><div class='message-text'>";
     var megContainer2 = "</div><span class='message-time pull-right'>";
     var megContainer3 = "</span></div></div></div>"
@@ -38,16 +36,14 @@ handleSubmit = () => {
     var time = d.getDate()+"-"+(d.getMonth() + 1)+"-"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
     console.log(time);
     $('textarea').val('');
-    console.log(mesg);
-    console.log('ss')
+//    console.log(mesg);
 //    make object
     var sendMsg = new Object();
     sendMsg.topicId = 1;
     sendMsg.speakerId = "";
     sendMsg.type = 1;
     sendMsg.content = mesg;
-
-    console.log(sendMsg);
+//    console.log(sendMsg);
 
 //    post json
     $.ajax({
@@ -61,14 +57,11 @@ handleSubmit = () => {
         console.log(data);
         console.log(status);
         handleResponse(data);
-//        alert('success');
     	},
     	error: function (e) {
     	    alert('Error, try again')
             }
     	});
-
-
 
     $("#conversation").append(megContainer1 + mesg + megContainer2 + time + megContainer3);
     $('#conversation').animate({
@@ -91,21 +84,3 @@ $("#robotIcon").click(function(){
     $('#conversation').animate({
         scrollTop: $('#conversation').get(0).scrollHeight}, 300); 
 })
-
-//$(document).ready(function() {
-//    $.ajax({
-//            type: "GET",
-//        	contentType: "application/json",
-//        	url: "/sendMsg",
-//        	data: JSON.stringify(sendMsg),
-//        	dataType: 'json',
-//        	timeout: 600,
-//            success: function (data) {
-//            alert('success');
-//        	},
-//        	error: function (e) {
-//        	    alert('err')
-//                }
-//        	});
-//
-//});
